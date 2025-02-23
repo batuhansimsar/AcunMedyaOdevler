ded60891f2715f3d7759d1e8e0700a9d80cdd4b1
batuhan <110763853+batuhansimsar@users.noreply.github.com>
2025-02-23 09:57:43 +0300

    Update soru2.cs

--- odev2/soru2.cs
+++ odev2/soru2.cs
@@ -1,11 +1,11 @@
-int number = 0;
-do{
-    Console.WriteLine("Lütfen 10 ile 100 arasında bir sayı giriniz: ");
-    number = Convert.ToInt32(Console.ReadLine());
-    if (number<10 || number>=100){
-        Console.WriteLine("Girilen sayı 10 ile 100 arasında olmalıdır.");
-    }
-    else{
-        Console.WriteLine("Girilen sayı: "+number);
-    }
-} while (number<10 || number>=100);
+        Console.Write("Lütfen 10 ile 100 arasında bir sayı giriniz: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        while (number <= 10 || number >= 100)
+        {
+            Console.WriteLine("Girilen sayı 10 ile 100 arasında olmalıdır.");
+            Console.Write("Lütfen tekrar giriniz: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Girilen sayı: " + number);
