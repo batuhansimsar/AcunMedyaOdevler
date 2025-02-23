82c7e171f2099ea12b4f8520a029b0b071a8af7a
batuhan <110763853+batuhansimsar@users.noreply.github.com>
2025-02-23 09:58:45 +0300

    Update soru2.cs

--- odev2/soru2.cs
+++ odev2/soru2.cs
@@ -1,7 +1,7 @@
         Console.Write("Lütfen 10 ile 100 arasında bir sayı giriniz: ");
         int number = Convert.ToInt32(Console.ReadLine());
 
-        while (number < 10 || number > 100)
+        while (number <= 10 || number >= 100)
         {
             Console.WriteLine("Girilen sayı 10 ile 100 arasında olmalıdır.");
             Console.Write("Lütfen tekrar giriniz: ");
